76ca9b2b2b7bb7ba02585811fb4e3c960aa83203
becca09 <adeolaadefuye@gmail.com>
2022-06-19 12:30:25 +0100

    first commit

---
+++ src/tictactoe/Player.java
@@ -0,0 +1,23 @@
+package tictactoe;
+
+public class Player {
+    private Elements element;
+    private String name;
+
+    public Player(String playerName, String playerElement) {
+        name = playerName;
+        element = Elements.valueOf(playerElement);
+    }
+
+    public void play(int position, Board board){
+        board.setElements(element, position);
+    }
+
+    public Elements getElement() {
+        return element;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ test/TicTacToeTes.java
@@ -0,0 +1,27 @@
+public class TicTacToeTes {
+    //package chapterEight.tictacto;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//public class TicTacToTest {
+//    private TicTacTo ticTacTo;
+//    private Elements elements;
+//    private Player players;
+//
+//    @BeforeEach public void setUo(){
+//       ticTacTo = new TicTacTo();
+//       players = new Player();
+//
+//    }
+//    @Test
+//    public void testThatTicTacToExist(){
+//        assertNotNull(ticTacTo);
+//    }
+//    @Test public void elementsExist(){
+//        assertNotNull(ticTacTo.getBoard());
+//    }
+}
